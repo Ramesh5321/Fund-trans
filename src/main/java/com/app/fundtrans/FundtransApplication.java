2232634bdae5b7251b79cd149f24415610d1fab2
Ramesh5321 <rameshgorla.672@gmail.com>
2021-05-28 19:20:59 +0530

    init commit

---
+++ src/main/java/com/app/fundtrans/FundtransApplication.java
@@ -0,0 +1,13 @@
+package com.app.fundtrans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundtransApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundtransApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/fundtrans/FundtransApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.fundtrans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundtransApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
